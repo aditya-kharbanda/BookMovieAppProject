fef47f638c9cb909a79a50c21d9c476f5273512e
Aditya Kharbanda <adityakharbanda@Adityas-MacBook-Air.local>
2022-04-05 21:43:11 +0530

    controller corrections

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,33 +1,30 @@
 import React from "react";
-// import Home from "../screens/home/Home";
-// import Details from "../screens/details/Details";
+import Home from "../screens/home/Home";
+import Details from "../screens/details/Details";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// import BookShow from "../screens/bookshow/BookShow";
+import BookShow from "../screens/bookshow/BookShow";
 import Confirmation from "../screens/confirmation/Confirmation";
 
 const Controller = () => {
-  const baseUrl = "/api/v1/";
+  const baseUrl = "/";
+  const movieDetailsUrl = "movie-details/:id";
+  const bookShowUrl = "/bookshow/:id";
 
   return (
     <Router>
       <div className="main-container">
-        <Route
-          exact
-          path="/"
-          render={(props) => <Home {...props} baseUrl={baseUrl} />}
-        />
-        <Route
-          path="/movie/:id"
-          render={(props) => <Details {...props} baseUrl={baseUrl} />}
-        />
-        <Route
+        <Route path="/" exact component={Home} />
+        <Route path="/movie-details/:id" component={Details} />
+        <Route path="/book-show/:id" component={BookShow} />
+        
+        {/* <Route
           path="/bookshow/:id"
           render={(props) => <BookShow {...props} baseUrl={baseUrl} />}
         />
         <Route
           path="/confirm/:id"
           render={(props) => <Confirmation {...props} baseUrl={baseUrl} />}
-        />
+        />  */}
       </div>
     </Router>
   );
